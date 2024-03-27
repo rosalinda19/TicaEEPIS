f050bc24e3af8c930521508fec64458a3bec94a1
ex-dec <bighadi123@yahoo.com>
2024-03-27 20:46:03 +0700

    update: page title

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/bukti', function () {
+    return view('Front/Bukti-publikasi');
+});
+
+require __DIR__ . '/auth.php';
