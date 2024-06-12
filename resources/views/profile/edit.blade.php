da9aa8005b7810a14eddc5a5016791c70668149a
ROSALINDA D.V <100073740+rosalinda19@users.noreply.github.com>
2024-06-13 00:19:25 +0700

    profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,28 +9,36 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    <div class="flex">
-            <img class="block h-500 m-10 w-auto fill-current text-gray-800 dark:text-gray-200 flex items-center justify-center" src="/assets/img/FaUserGraduate.png">
+    <div class="flex items-center justify-center">
+            <img class="block h-45 m-10" src="/assets/img/FaUserGraduate.png">
     </div>
     <div class="max-w-sm flex items-center justify-center"
         style="max-width: 25vw; min-height: 60vh; display: flex; background-color: lightgrey;">
         <div class="px-12 py-4" style="align-content: center;">
-            <div class="font-bold text-xl mb-2">Selamat Datang di Tica</div>
-            <p class="text-gray-700 text-base">
-                Program Pascasarjana Politeknik Elektronika Negeri Surabaya membuka Penerimaan Mahasiswa Baru
-                Tahun 2023 Gelombang 2, bagi Program Magister Terapan pada bidang Teknologi Rekayasa untuk
-                Program Studi S2 Terapan Teknik Elektro dan Program Studi S2 Terapan Teknik Informatika dan
-                Komputer.
-            </p>
-            <p class="font-bold">
-                Login mahasiswa menggunakan E-Mail
-            </p>
-            <p class="font-bold">
-                @xx.student.pens.ac.id
-            </p>
-            <p class="font-bold">
-                xx=kode jurusan
-            </p>
+            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                <div class="md:col-span-5">
+                    <label for="">NRP</label>
+                    <input type="text" name="" id=""
+                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                </div>
+                <div class="md:col-span-5">
+                    <label for="">Angkatan</label>
+                    <input type="text" name="" id=""
+                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                </div>
+                <div class="md:col-span-5">
+                    <label for="">Program Study</label>
+                    <input type="text" name="" id=""
+                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
+                </div>
+                <div class="md:col-span-5 text-center">
+                    <div class="inline-flex items-end">
+                        <button
+                            class="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            type="button">Exit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </x-guest-layout>
